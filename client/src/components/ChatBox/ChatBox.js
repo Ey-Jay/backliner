de2bdb8d532242788bb213fee3c9762a3115f4fd
Joss Doebler <mail@joss-doebler.de>
2020-09-25 17:17:46 +0200

    Fixed 404 error on socket

--- client/src/components/ChatBox/ChatBox.js
+++ client/src/components/ChatBox/ChatBox.js
@@ -24,7 +24,7 @@ const chatMessages = [
   { message: 'nm and you?', isMine: false },
 ];
 
-const socket = io('/');
+const socket = io('http://localhost:3001');
 
 const ChatBox = ({ isOpen, setIsOpen }) => {
   const [message, setMessage] = useState('');
