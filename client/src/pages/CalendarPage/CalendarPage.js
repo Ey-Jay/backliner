6e39c7fe9f86e1f2dde7dcee806025ba9ed7e741
Erick Jansen <janseniter@gmail.com>
2020-10-06 16:15:07 +0200

    desktop calendar

--- client/src/pages/CalendarPage/CalendarPage.js
+++ client/src/pages/CalendarPage/CalendarPage.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import './styles.css';
 import moment from 'moment';
 import { useParams } from 'react-router-dom';
 import { GlobalContext } from 'context/GlobalContext';
 import axios from 'axios';
 import Spinner from 'components/Spinner';
+import { ThemeContext } from 'styled-components';
 
 import Layout from 'layout';
-import { Header } from 'layout/Layout.style';
+import { CalendarToolbar, NavigationButton } from './CalendarPage.style';
 
 const localizer = momentLocalizer(moment);
 
@@ -18,17 +20,52 @@ const CalendarPage = () => {
   const [calendarEvents, setCalendarEvents] = useState(null);
   const [calendarAuthorized, setCalendarAuthorized] = useState(false);
   const [loading, setLoading] = useState(true);
+  const theme = useContext(ThemeContext);
 
   let events;
   if (calendarEvents) {
     events = calendarEvents.map((event) => ({
       title: event.summary,
-      start: event.start.dateTime,
-      end: event.end.dateTime,
+      start: event.start.dateTime || event.start.date,
+      end: event.end.dateTime || event.end.date,
     }));
   }
 
-  console.log(events);
+  const CustomToolbar = (props) => {
+    return (
+      <CalendarToolbar>
+        <h1>{props.label}</h1>
+        <div>
+          <NavigationButton onClick={() => props.onNavigate('TODAY')}>
+            Today
+          </NavigationButton>
+          <NavigationButton onClick={() => props.onNavigate('PREV')}>
+            Back
+          </NavigationButton>
+          <NavigationButton onClick={() => props.onNavigate('NEXT')}>
+            Next
+          </NavigationButton>
+        </div>
+      </CalendarToolbar>
+    );
+  };
+
+  function dayStyleGetter(day) {
+    const style = {
+      backgroundColor: 'inherit',
+    };
+    if (moment(Date.now()).isSame(day, 'day')) return { style };
+    if (!moment(Date.now()).isSame(day, 'month'))
+      return {
+        style: {
+          backgroundColor: 'inherit',
+        },
+      };
+  }
+
+  const eventStyleGetter = () => ({
+    style: { backgroundColor: theme.primary, outline: 'none' },
+  });
 
   useEffect(() => {
     currentUser
@@ -71,6 +108,12 @@ const CalendarPage = () => {
           style={{ height: '100%', padding: '40px' }}
           toolbar={true}
           popup={true}
+          showMultiDayTimes={true}
+          dayPropGetter={dayStyleGetter}
+          eventPropGetter={eventStyleGetter}
+          components={{
+            toolbar: CustomToolbar,
+          }}
         />
       )}
     </Layout>
--- client/src/pages/CalendarPage/CalendarPage.style.js
+++ client/src/pages/CalendarPage/CalendarPage.style.js
@@ -1,3 +1,31 @@
-import style from 'styled-components';
+import styled from 'styled-components';
 
 export const Container = styled.div``;
+
+export const CalendarToolbar = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const NavigationButton = styled.button`
+  background-color: ${({ theme }) => theme.secondary};
+  color: ${({ theme }) => theme.color};
+  border: none;
+  padding: 10px 15px;
+  margin: 2px;
+  cursor: pointer;
+  border-radius: 5px;
+  outline: none;
+  text-transform: uppercase;
+  transition: all 0.2s;
+
+  &:hover {
+    background-color: ${(props) => props.theme.secondaryDark};
+  }
+
+  &:active  {
+    transition: all 0.1s;
+    transform: scale(0.9);
+  }
+`;
