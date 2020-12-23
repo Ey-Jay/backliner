ffe76f8d298d735ce23b58427607b5498b74afc6
Joss Doebler <mail@joss-doebler.de>
2020-12-23 18:24:15 +0100

    Add theme and GlobalStyle

--- app/style/GlobalStyle.js
+++ app/style/GlobalStyle.js
@@ -1,5 +1,28 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, css } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle``;
+const GlobalStyle = createGlobalStyle`
+${({ theme }) => css`
+  * {
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Inter', sans-serif;
+  }
+
+  body {
+    background-color: ${theme.backgroundColor};
+    color: ${theme.color};
+  }
+
+  a {
+    text-decoration: inherit;
+    color: inherit;
+  }
+`}
+`;
 
 export default GlobalStyle;
--- app/style/theme.js
+++ app/style/theme.js
@@ -1,3 +1,13 @@
-const theme = {};
+const theme = {
+  primary: '#FF4136',
+  primaryDark: '#E00B00',
+  secondary: '#888888',
+  secondaryDark: '#777777',
+  color: '#FFFCF9',
+  backgroundColor: '#111111',
+  backgroundColorDark: '#0a0a0a',
+  backgroundColorLight: '#333333',
+  breakpointMobile: '700px',
+};
 
 export default theme;
