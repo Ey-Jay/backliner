743657888987fe69b77cc9de3ca98a54bb80cfe7
Joss Doebler <mail@joss-doebler.de>
2020-12-24 01:57:42 +0100

    Fix error message for [bid] route

--- app/pages/[bid]/index.js
+++ app/pages/[bid]/index.js
@@ -3,6 +3,7 @@ const BandIndexPage = () => <div>Loading ...</div>;
 export async function getServerSideProps({ req, params }) {
   // check if user is valid
   // check if user is in band
+  return { props: {} };
 }
 
 export default BandIndexPage;
