539b6781b010ee31efa8733d92c5c3229a119f85
Joss Doebler <mail@joss-doebler.de>
2020-12-24 01:33:23 +0100

    Fix routing error message

--- app/pages/[bid]/index.js
+++ app/pages/[bid]/index.js
@@ -1,1 +1,8 @@
-// redirect to /[bid]/projects
+const BandIndexPage = () => <div>Loading ...</div>;
+
+export async function getServerSideProps({ req, params }) {
+  // check if user is valid
+  // check if user is in band
+}
+
+export default BandIndexPage;
