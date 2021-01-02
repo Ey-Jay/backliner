0b48c9cd397997af7a4411c4a072cd7d37a94224
Joss Doebler <mail@joss-doebler.de>
2021-01-02 14:15:33 +0100

    Clarify firebase auth instruction

--- app/middleware/withAuthAndDb.js
+++ app/middleware/withAuthAndDb.js
@@ -4,6 +4,7 @@ import getDbUser from '@utils/db/getDbUser';
 
 // We can't import verifyIdToken from firebase directly because of a library bug
 // https://github.com/firebase/firebase-functions/issues/597
+// It needs to be handed to the middleware inside of a serverside function
 const withAuthAndDb = async (req, verifyToken) => {
   await dbConnect();
   req.authToken = parseCookiesServerSide(req.headers.cookie).auth.token;
